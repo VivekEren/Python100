487acfd7abc6663451c81b1e9e43865fafbac585
VivekEren <81611911+VivekEren@users.noreply.github.com>
2021-03-30 12:22:58 +0530

    Create band_name_generator.py

---
+++ Day1_BandNameGenerator/band_name_generator.py
@@ -0,0 +1,10 @@
+"""
+Band Name Generator Simple Project
+Get the city name where user was born and nick name of user
+Add those two strings together to form a band name
+"""
+print("Welcome to the Band Name Generator")
+city_name = input("Enter the city where you were born:\n")
+nick_name = input("Enter your nick name:\n")
+band_name = city_name + " " + nick_name
+print("Your band name will be: " + band_name)
