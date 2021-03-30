99620d32313c114d09247446ea16f663074ba876
VivekEren <81611911+VivekEren@users.noreply.github.com>
2021-03-31 03:07:21 +0530

    Create rock_paper_scissors.py

---
+++ Day4_RockPaperScissors/rock_paper_scissors.py
@@ -0,0 +1,56 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+choice = [rock, paper, scissors]
+
+print("Let's play rock, paper and scissors")
+
+user_choice = int(input("Choose 0 for rock, 1 for paper and 2 for scissors\n"))
+
+computer_choice = random.randint(0,2)
+print("Your Choice:")
+print(choice[user_choice])
+print("Computer Choice:")
+print(choice[computer_choice])
+
+if user_choice == 0 and computer_choice == 1:
+    print("You Lost")
+elif user_choice == 0 and computer_choice == 2:
+    print("You Won")
+elif user_choice == 1 and computer_choice == 0:
+    print("You Won")
+elif user_choice == 1 and computer_choice == 2:
+    print("You Lost")
+elif user_choice == 2 and computer_choice == 0:
+    print("You Lost")
+elif user_choice == 2 and computer_choice == 1:
+    print("You Won")
+else:
+    print("It's a draw")
+
